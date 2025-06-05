4e8d70da169050d2024a79d755f917d36e5a21e1
osama2kabdullah <abdullah21673@hotmail.com>
2025-06-05 14:18:41 +0600

    make template reusable and added example of secret files

--- app/routes.py
+++ app/routes.py
@@ -5,3 +5,7 @@ main = Blueprint('main', __name__)
 @main.route('/')
 def index():
     return render_template('index.html')
+
+@main.route('/login')
+def login():
+    return render_template('login.html')
